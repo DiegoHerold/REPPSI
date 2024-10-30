fe81c794b847adc5588238d25fcdaf4ef82d6550
DiegoHerold <123795451+DiegoHerold@users.noreply.github.com>
2024-10-30 06:12:00 -0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -4,8 +4,3 @@ const app = require("../src/api");
 app.use((req,res,next)=>{
     next();
 });
-console.log(process.env.API_PORT);
-let port = process.env.API_PORT || 3001;
-app.listen(port);
-
-console.log(`listening on ${port}`);
